78747e4c819c56c488d26815024d9181011b2b12
Samuel Oberhofer <sa.oberhofer@mci4me.at>
2023-11-24 22:12:03 +0100

    write all results to single file

--- process_predictions.py
+++ process_predictions.py
@@ -8,7 +8,7 @@ prediction = os.path.join(latest_run, "predictions.csv")
 
 df = pd.read_csv(prediction,header=None)
 df.columns = ["video", "class", "confidence"]
-
+videos_data = []
 # iterate over every video
 for video in df["video"].unique():
     istrigger = False
@@ -17,29 +17,26 @@ for video in df["video"].unique():
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     duration = float(length)/fps
-    total_frames = length/24
+    total_frames = length/23
     # Get trigger frames
     trigger_frames = df[df["video"]==video].count()["video"]
     ratio = trigger_frames/total_frames
 
     # If more than half of the frames we looked at had a trigger, then it's a trigger exercise
     if ratio > 0.5:
-        print("Video: {} is trigger exercise".format(video))
         istrigger = True
-    else:
-        print("Video: {} is not trigger exercise".format(video))
     video_data = {
         "video": video,
         "isTrigger": istrigger,
-        "total_frames": fps,
+        "total_frames": length,
         "ratio": float(ratio),
         "duration": duration
     }
-    #print(type(video), type(istrigger), type(total_frames), type(trigger_frames), type(ratio), type(duration))
-    # Serializing json
-    json_object = json.dumps(video_data, indent=4)
- 
-    # Writing to sample.json
-    with open("../videos/"+video+".json", "w") as outfile:
-        outfile.write(json_object)
+    videos_data.append(video_data)
+# Serializing json
+json_object = json.dumps(videos_data, indent=4)
+
+# Writing to sample.json
+with open("../videos/results.json", "w") as outfile:
+    outfile.write(json_object)
     
