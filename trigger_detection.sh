76880b86abb8486babefff1437966a2a6ed0d21d
Samuel Oberhofer <sa.oberhofer@mci4me.at>
2023-11-30 21:48:43 +0100

    fix missing #

--- trigger_detection.sh
+++ trigger_detection.sh
@@ -1,9 +1,9 @@
-Put the videos you want to process in the videos folder.
+#Put the videos you want to process in the videos folder.
 git clone https://github.com/ultralytics/yolov5  # clone
 cd yolov5
 pip install -qr requirements.txt  # install
 echo "Running inference on videos..."
-VID_STRIDE=15
+VID_STRIDE=24
 python detect.py --weights ../models/various_detection_colab_100ep.pt --source ../videos --vid-stride $VID_STRIDE --save-csv
 python ../process_predictions.py $VID_STRIDE
 python ../processing_rawdata.py
